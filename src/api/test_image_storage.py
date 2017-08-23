c288451f27f9b24d9f0866d89af030215e42f45d
Christian Burke <cburke91@live.com>
2017-08-22 23:28:43 -0700

    changed .env to an example file

--- src/api/test_image_storage.py
+++ src/api/test_image_storage.py
@@ -1,14 +1,20 @@
 
 import unittest
+import s3fs
+import os
 
-class TestStringMethods(unittest.TestCase):
+class TestBucketOperations(unittest.TestCase):
 
-    def testBucketCreation(self):
+    def testEnvironmentVariables(self):
+        self.assertNotEqual(os.environ['S3_KEY'], 'keyhere')
+        self.assertEqual(os.environ['TEST_ENV_VAR'], 'testing')
+
+    def testFileCreation(self):
         s3 = s3fs.S3FileSystem(anon=False, key=os.environ['S3_KEY'], secret=os.environ['S3_SECRET'])
         with s3.open(os.environ['S3_BUCKET'] + "/testfile_klasjflkjasflk", 'wb') as file:
             file.write(b'helloworld');
         with s3.open(os.environ['S3_BUCKET'] + "/testfile_klasjflkjasflk", 'rb') as file:
-            assertEqual(file.read() == b'helloworld')
+            self.assertEqual(file.read() == b'helloworld')
 
 if __name__ == '__main__':
     unittest.main()
